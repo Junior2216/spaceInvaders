a4ee3a86fd51dcaad32a59a7c7dab8d7f270fc04
iut <iut@10.0.2.15>
2017-04-14 11:59:43 +0200

    2nd commit

---
+++ src/main/java/fr/unilim/iut/spaceInvader/SpaceInvaders.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.spaceInvader;
+  
+    public class SpaceInvaders {
+
+	    int longueur;
+	    int hauteur;
+
+	    public SpaceInvaders(int longueur, int hauteur) {
+		   this.longueur = longueur;
+		   this.hauteur = hauteur;
+	   }
+
+   }
