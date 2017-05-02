52486b43c1aaf14ebf4a2e4563ed9ff58bc1cc41
iut <iut@10.0.2.15>
2017-05-02 08:38:17 +0200

    1/2 sprint 1

--- src/main/java/fr/unilim/iut/spaceInvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceInvader/SpaceInvaders.java
@@ -4,10 +4,41 @@ package fr.unilim.iut.spaceInvader;
 
 	    int longueur;
 	    int hauteur;
+	    Vaisseau vaisseau;
 
+	    
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
 		   this.hauteur = hauteur;
 	   }
+	    @Override
+		public String toString() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
+				}
+				espaceDeJeu.append('\n');
+			}
+			return espaceDeJeu.toString();
+		}
+	    private boolean aUnVaisseau(){
+	    	return vaisseau!=null;
+	    }
+	    private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y){
+	    	return ( this.aUnVaisseau() && this.vaisseau.occupeLaPosition(x, y));
+	    }
+	    private char recupererMarqueDeLaPosition(int x, int y) {
+	        char marque;
+	        if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+	           marque='V';
+	        else
+	           marque='.';
+	        return marque;
+		}
+	    
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+	        this.vaisseau = new Vaisseau(x, y);
 
+		}
    }
